baf9625b029339fe463900ee1287a4b0daf21aa8
srvinith <r36041595@gmail.com>
2023-11-10 00:09:45 +0530

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className='hero' id='hero'>
+            <div className='content'>
+                <p></p>
+                <p></p>
+                <p>Immersive Solutions</p>
+                <p>Transforming the way you experience the digital world</p>
+                <button href='/' className='button'>Free Consultation</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
